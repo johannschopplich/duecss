c757efc167e15d4eab31446680a4ed48707dfba1
Johann Schopplich <mail@johannschopplich.com>
2021-11-14 18:23:30 +0100

    refactor: remove box as default shortcut

--- src/shortcuts.ts
+++ src/shortcuts.ts
@@ -2,6 +2,5 @@ import type { UserShortcuts } from 'unocss'
 
 export const shortcuts: UserShortcuts = {
   section: 'py-13',
-  box: 'block bg-[var(--color-background)] rounded-[var(--due-radius)] p-[var(--due-spacer)]',
   title: 'text-contrast-higher font-heading font-600 leading-heading'
 }

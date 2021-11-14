0b98926989e07e359364bbaa38b237fafe95b957
Johann Schopplich <mail@johannschopplich.com>
2021-11-14 23:27:27 +0100

    feat: dynamic columns utilities

--- src/shortcuts.ts
+++ src/shortcuts.ts
@@ -1,7 +1,15 @@
 import type { UserShortcuts } from 'unocss'
 
-export const shortcuts: UserShortcuts = {
-  section: 'py-13',
-  title: 'text-contrast-higher font-heading font-600 leading-heading',
-  'centered-content': 'flex flex-col items-center justify-center'
-}
+export const shortcuts: UserShortcuts = [
+  {
+    section: 'py-13',
+    title: 'text-contrast-higher font-heading font-600 leading-heading',
+    'centered-content': 'flex flex-col items-center justify-center',
+    columns: 'flex flex-wrap',
+    column: 'block flex-1',
+    'column-narrow': 'block flex-grow-0 flex-shrink-0 flex-basis-auto w-auto',
+    'column-auto': 'block flex-1 w-auto',
+    'column-full': 'block flex-none w-full'
+  },
+  [/^column-(\d+)$/, ([, d]) => `flex-none w-${d}/12`]
+]

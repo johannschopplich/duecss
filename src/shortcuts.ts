da559091bfd65fdf24b5e633d4e2a73e33a88270
Johann Schopplich <mail@johannschopplich.com>
2021-11-14 18:04:54 +0100

    fix: title font weight

--- src/shortcuts.ts
+++ src/shortcuts.ts
@@ -3,5 +3,5 @@ import type { UserShortcuts } from 'unocss'
 export const shortcuts: UserShortcuts = {
   section: 'py-13',
   box: 'block bg-[var(--color-background)] rounded-[var(--due-radius)] p-[var(--due-spacer)]',
-  title: 'text-contrast-higher font-heading leading-heading weight-600'
+  title: 'text-contrast-higher font-heading font-600 leading-heading'
 }

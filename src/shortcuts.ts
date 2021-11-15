415064207f4772b79a664ddf1d11b30c0f163caf
Johann Schopplich <mail@johannschopplich.com>
2021-11-15 09:38:57 +0100

    fix: add default container class

--- src/shortcuts.ts
+++ src/shortcuts.ts
@@ -2,15 +2,16 @@ import type { UserShortcuts } from 'unocss'
 
 export const shortcuts: UserShortcuts = [
   [
-    /^container-([a-z]+)$/,
+    /^container-(sm|md|lg|xl|2xl)$/,
     ([, bp]) => `relative max-w-screen-${bp} mx-auto px-[var(--due-spacer)]`
   ],
   [
-    /^container-([a-z]+)-full$/,
+    /^container-(sm|md|lg|xl|2xl)-full$/,
     ([, bp]) => `relative max-w-screen-${bp} mx-auto`
   ],
   [/^column-(\d+)$/, ([, d]) => `flex-none w-${d}/12`],
   {
+    container: 'relative mx-auto px-[var(--due-spacer)]',
     section: 'py-13',
     title: 'text-contrast-higher font-heading font-600 leading-heading',
     'centered-content': 'flex flex-col items-center justify-center',

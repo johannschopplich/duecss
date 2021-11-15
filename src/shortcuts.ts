421a86039bc58593934c0f1d4d8a8fbf4827442c
Johann Schopplich <mail@johannschopplich.com>
2021-11-15 09:34:18 +0100

    feat: container shortcuts

--- src/shortcuts.ts
+++ src/shortcuts.ts
@@ -1,6 +1,15 @@
 import type { UserShortcuts } from 'unocss'
 
 export const shortcuts: UserShortcuts = [
+  [
+    /^container-([a-z]+)$/,
+    ([, bp]) => `relative max-w-screen-${bp} mx-auto px-[var(--due-spacer)]`
+  ],
+  [
+    /^container-([a-z]+)-full$/,
+    ([, bp]) => `relative max-w-screen-${bp} mx-auto`
+  ],
+  [/^column-(\d+)$/, ([, d]) => `flex-none w-${d}/12`],
   {
     section: 'py-13',
     title: 'text-contrast-higher font-heading font-600 leading-heading',
@@ -10,6 +19,5 @@ export const shortcuts: UserShortcuts = [
     'column-narrow': 'block flex-grow-0 flex-shrink-0 flex-basis-auto w-auto',
     'column-auto': 'block flex-1 w-auto',
     'column-full': 'block flex-none w-full'
-  },
-  [/^column-(\d+)$/, ([, d]) => `flex-none w-${d}/12`]
+  }
 ]

1c2d9dc1de0def992db20fe7861966d3f11b9bd0
Johann Schopplich <mail@johannschopplich.com>
2021-11-16 13:01:39 +0100

    fix: disabled button opacity

--- src/shortcuts.ts
+++ src/shortcuts.ts
@@ -1,7 +1,7 @@
 import type { UserShortcuts } from 'unocss'
 
 const buttonBase =
-  'due-rounded appearance-none unselectable inline-flex items-center justify-center border border-transparent leading-none px-3 py-2 disabled:opacity-50 disabled:text-theme-text'
+  'due-rounded appearance-none unselectable inline-flex items-center justify-center border border-transparent leading-none px-3 py-2 disabled:opacity-50'
 
 export const shortcuts: UserShortcuts = [
   [

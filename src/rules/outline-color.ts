34abc599b8d238b964e3ac7a9f73bdf7cf83539c
Johann Schopplich <mail@johannschopplich.com>
2021-11-18 08:27:13 +0100

    chore: new color resolver util

--- src/rules/outline-color.ts
+++ src/rules/outline-color.ts
@@ -1,15 +1,6 @@
+import { colorResolver } from '../utils'
 import type { Rule } from 'unocss'
 
 export const outlineColor: Rule[] = [
-  [
-    /^outline-color-(\w+)$/,
-    ([, name], { rawSelector, currentSelector, variantHandlers, theme }) => {
-      // @ts-expect-error: colors not defined
-      const colorData = theme.colors?.[name]
-      const color =
-        typeof colorData === 'string' ? colorData : colorData.DEFAULT
-
-      return { 'outline-color': color }
-    }
-  ]
+  [/^outline-color-(\w+)$/, colorResolver('outline-color')]
 ]
---
+++ src/utils.ts
@@ -0,0 +1,46 @@
+import type { Theme } from './theme'
+import type { RuleContext } from 'unocss'
+
+export function parseBracket(str: string) {
+  if (str && str[0] === '[' && str[str.length - 1] === ']')
+    return str.slice(1, -1).replace(/_/g, ' ')
+}
+
+export const parseColor = (body: string, theme: Theme) => {
+  const [name, no = 'DEFAULT'] = body
+    .replace(/([a-z])([0-9])/g, '$1-$2')
+    .split(/-/g)
+
+  if (!name) return
+
+  let color: string | undefined
+  const bracket = parseBracket(body) || body
+  if (bracket.startsWith('#')) color = bracket.slice(1)
+  if (bracket.startsWith('hex-')) color = bracket.slice(4)
+
+  if (!color) {
+    const colorData = theme.colors?.[name]
+    if (typeof colorData === 'string') color = colorData
+    else if (no && colorData) color = colorData[no]
+  }
+
+  return {
+    name,
+    no,
+    color
+  }
+}
+
+export const colorResolver =
+  (attribute: string) =>
+  ([, body]: string[], { theme }: RuleContext<Theme>) => {
+    const data = parseColor(body, theme)
+    if (!data) return
+
+    const { color } = data
+    if (!color) return
+
+    return {
+      [attribute]: color
+    }
+  }

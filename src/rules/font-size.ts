066cc8a463e2994dfa6e628b5c5a16339b2f79cc
Johann Schopplich <mail@johannschopplich.com>
2021-11-18 08:52:32 +0100

    fix: word with font size rule

--- src/rules/font-size.ts
+++ src/rules/font-size.ts
@@ -1,5 +1,5 @@
 import type { Rule } from 'unocss'
 
 export const fontSize: Rule[] = [
-  [/^font-size-(\d+)$/, ([, d]) => ({ 'font-size': `var(--text-${d})` })]
+  [/^font-size-(\w+)$/, ([, w]) => ({ 'font-size': `var(--text-${w})` })]
 ]

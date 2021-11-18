a74ed4b6a2efad279a279842a3d0057af9414f38
Johann Schopplich <mail@johannschopplich.com>
2021-11-18 10:26:22 +0100

    feat: due spacing shortcut

--- src/shortcuts.ts
+++ src/shortcuts.ts
@@ -3,22 +3,33 @@ import type { UserShortcuts } from 'unocss'
 const buttonBase =
   'due-rounded appearance-none unselectable inline-flex items-center justify-center border border-transparent leading-none px-3 py-2 disabled:opacity-50'
 
+const spacingMap: Record<string, string> = {
+  '2xs': '1',
+  xs: '2',
+  s: '3',
+  m: '5',
+  l: '8',
+  xl: '13',
+  '2xl': '21',
+  '3xl': '34'
+}
+
 export const shortcuts: UserShortcuts = [
   [
     /^container(?:-(fluid))?$/,
     ([, m]) =>
-      `relative mx-${m ? '[var(--due-spacer)]' : 'auto due-w-container'}`
+      `relative mx-${m ? '[var(--du-spacer)]' : 'auto due-w-container'}`
   ],
   [
     /^container-(sm|md|lg|xl|2xl)(?:-(full|fluid))?$/,
     ([, bp, m]) =>
-      `relative mx-${m === 'fluid' ? '[var(--due-spacer)]' : 'auto'} ${
+      `relative mx-${m === 'fluid' ? '[var(--du-spacer)]' : 'auto'} ${
         m === 'full' ? 'w-full' : m !== 'fluid' ? 'due-w-container' : ''
       } max-w-screen-${bp}`
   ],
   [/^column-(\d+)$/, ([, d]) => `flex-none w-${d}/12`],
+  [/^due-([pm][xyrltbse]?)-(.+)$/, ([, d, v]) => `${d}-${spacingMap[v]}`],
   {
-    section: 'py-13',
     title: 'text-contrast-higher font-heading font-600 leading-heading',
     columns: 'flex flex-wrap',
     column: 'block flex-1',

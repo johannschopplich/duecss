1e2bf2e6efabe686f3d5127ca71082babfb61cb4
Johann Schopplich <mail@johannschopplich.com>
2021-12-10 12:04:16 +0100

    refactor: compat for due-text-size

--- src/rules/due.ts
+++ src/rules/due.ts
@@ -3,7 +3,10 @@ import type { Rule } from '@unocss/core'
 
 export const due: Rule[] = [
   [/^due?-rounded$/, () => ({ 'border-radius': 'var(--du-radius)' })],
-  [/^due?-text-(\w+)$/, ([, m]) => ({ 'font-size': `var(--du-text-${m})` })],
+  [
+    /^due?-text-(?:size-)(\w+)$/,
+    ([, w]) => ({ 'font-size': `var(--du-text-${w})` })
+  ],
   [
     /^due?-var-color-(.+)$/,
     ([, body], { rawSelector, currentSelector, variantHandlers, theme }) => {

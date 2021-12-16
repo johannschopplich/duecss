23a836fff84a4b3aa9daf1ba236cdb26e40b6c0f
Johann Schopplich <mail@johannschopplich.com>
2021-12-16 12:47:18 +0100

    chore: emit cjs for jiti

--- build.config.ts
+++ build.config.ts
@@ -3,5 +3,8 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   entries: ['src/index'],
   clean: true,
-  declaration: true
+  declaration: true,
+  rollup: {
+    emitCJS: true
+  }
 })

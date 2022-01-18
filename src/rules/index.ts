fd8a34b12342f05b0b7721880ee6eea38c837b85
Johann Schopplich <mail@johannschopplich.com>
2022-01-18 10:33:23 +0100

    feat!: drop stretched link

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -2,7 +2,6 @@ import { content } from './content'
 import { due } from './due'
 import { hyphenated } from './hyphenated'
 import { outlineColor } from './outline-color'
-import { stretchedLink } from './stretched-link'
 import { textDecorationLine } from './text-decoration-line'
 import { unselectable } from './unselectable'
 import type { Rule } from '@unocss/core'
@@ -12,7 +11,6 @@ export const rules: Rule[] = [
   due,
   hyphenated,
   outlineColor,
-  stretchedLink,
   textDecorationLine,
   unselectable
 ].flat(1)

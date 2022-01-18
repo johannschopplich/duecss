85583738b845dc277e77849005543075e860da10
Johann Schopplich <mail@johannschopplich.com>
2022-01-18 10:51:17 +0100

    feat!: outline color now part of unocss

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,7 +1,6 @@
 import { content } from './content'
 import { due } from './due'
 import { hyphenated } from './hyphenated'
-import { outlineColor } from './outline-color'
 import { textDecorationLine } from './text-decoration-line'
 import { unselectable } from './unselectable'
 import type { Rule } from '@unocss/core'
@@ -10,7 +9,6 @@ export const rules: Rule[] = [
   content,
   due,
   hyphenated,
-  outlineColor,
   textDecorationLine,
   unselectable
 ].flat(1)
--- src/rules/outline-color.ts
+++
@@ -1,6 +0,0 @@
-import { colorResolver } from '../utils'
-import type { Rule } from '@unocss/core'
-
-export const outlineColor: Rule[] = [
-  [/^outline-color-(.+)$/, colorResolver('outline-color')]
-]

0d3e804344862148069a0259a57f7f4a3efa6445
Johann Schopplich <mail@johannschopplich.com>
2022-03-15 22:45:05 +0100

    refactor: rewrite masonry grid as shortcut

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,7 +1,6 @@
 import { due } from './due'
 import { hyphenate } from './hyphenate'
-import { masonryGrid } from './masonry-grid'
 import { unselectable } from './unselectable'
 import type { Rule } from '@unocss/core'
 
-export const rules: Rule[] = [due, hyphenate, masonryGrid, unselectable].flat(1)
+export const rules: Rule[] = [due, hyphenate, unselectable].flat(1)

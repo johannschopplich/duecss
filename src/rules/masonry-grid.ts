75bf7e8a00790d752549691c8098ba1a227a2fa4
Johann Schopplich <mail@johannschopplich.com>
2022-03-15 19:29:06 +0100

    fix: masonry grid item centering

--- src/rules/masonry-grid.ts
+++ src/rules/masonry-grid.ts
@@ -1,14 +1,24 @@
+import { escapeSelector as e } from '@unocss/core'
 import type { Rule } from '@unocss/core'
 
 export const masonryGrid: Rule[] = [
   [
-    'masonry-grid',
-    {
-      display: 'grid',
-      'grid-template-columns':
-        'repeat(auto-fit, minmax(min(var(--masonry-col-max-w, 25rem), 100%), 1fr))',
-      'grid-template-rows': 'masonry',
-      'justify-content': 'center'
+    /^masonry-grid$/,
+    ([,], { rawSelector }) => {
+      return `
+.${e(rawSelector)} {
+  display: grid;
+  grid-template-columns: repeat(
+    auto-fit,
+    minmax(min(var(--masonry-col-max-w, 25rem), 100%), 1fr)
+  );
+  grid-template-rows: masonry;
+  justify-content: center;
+}
+.${e(rawSelector)} > * {
+  align-self: start;
+}
+`
     }
   ]
 ]
